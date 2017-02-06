3517d43b1647a38b420a509381844924f99bfcb7
Ntrigued <virtfund@gmail.com>
2017-02-05 22:33:05 -0600

    Update OutputWindow.java

--- OutputWindow.java
+++ OutputWindow.java
@@ -33,6 +33,7 @@ public class OutputWindow extends JFrame {
 
 //        jTextArea1.setColumns(5);
  //       jTextArea1.setRows(5);
+        jTextArea1.setEditable(false);
         jTextArea1.setFont(new Font("monospaced", Font.PLAIN, 12));
         jScrollPane1.setViewportView(jTextArea1);
 
@@ -180,4 +181,88 @@ public class OutputWindow extends JFrame {
       }//End of Switch
     }//End of codeList method
     
+    /*
+     Converts characters from screen output to numeric values
+     */
+    public int reverseCodeList(char outputChar) {
+        switch(outputChar) {
+            case '0':
+                return 0;          
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'a':
+                return 10;
+            case 'b':
+                return 11;
+            case 'c':
+                return 12;
+            case 13:
+                return 13;
+            case 'e':
+                return 14;
+            case 'f':
+                return 15;
+            case 'g':
+                return 16;
+            case 'h':
+                return 17;
+            case 'i':
+                return 18;
+            case 'j':
+                return 19;
+            case 'k':
+                return 20;
+            case 'l':
+                return 21;
+            case 'm':
+                return 22;
+            case 'n':
+                return 23;
+            case 'o':
+                return 24;
+            case 'p':
+                return 25;
+            case 'q':
+                return 26;
+            case 'r':
+                return 27;
+            case 's':
+                return 28;
+            case 't':
+                return 29;
+            case 'u':
+                return 30;
+            case 'v':
+                return 31;
+            case 'w':
+                return 32;
+            case 'x':
+                return 33;
+            case 'y':
+                return 34;
+            case 'z':
+                return 35;
+            case '.':
+                return 36;
+                
+            default:
+            	return 37;
+        }//End of Switch
+      }//End of reverseCodeList method
 }
